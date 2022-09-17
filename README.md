f5c77fb56cf592c0cf98a2d8d664014996b7364e
MD. Muhiminul Haque <apon2041@gmail.com>
2022-09-17 23:00:41 +0600

    Update: General Object to Dom and implement Dom Builder

--- README.md
+++ README.md
@@ -2,37 +2,117 @@
 
 HTML Dom Builder
 
-## Inastalatio
+## Inastalation
 
-    `npm i @aponahmed/dombuilder`
+`npm i @aponahmed/dombuilder`
 
-## Uses
+## Import
+
+`import { Obj2Dom, Dombuilder } from "@aponahmed/dombuilder";`
+
+## Uses Dom Builder :: Dombuilder
 
 ```javascript
-import Dombuilder from "@aponahmed/dombuilder";
-let dom = new dombuilder({
+let div = new Dombuilder("div")
+  .attr("data-type", "hello Attr")
+  .class("div")
+  .event("click", () => {
+    console.log("Hello");
+  })
+  .html("Click to Hello")
+  .append(
+    new Dombuilder("button").html("Say Hello").event("click", () => {
+      div.html("Hello !");
+    }).element
+  );
+div.renderTo(document.getElementById("app"));
+```
+### Methods
+#### attr(AttributeName, Value) 
+To set Attributs in element
+#### class(className)  
+To add Class in element
+#### event(EventName,Callback)
+Bind Event with element, @param1 for event Name and @param2 for calback
+#### html(html)  
+Add Inner Html of element, @Param should be String
+#### appendHtml(html) 
+To Append Html after element inner Html
+#### append(DomElement)  
+To append Chield Element @Param should Dom element
+#### renderTo(DomElement)  
+To Output Dom Elements, @Param DomElement is output wraper element  
+
+## Uses object To Dom :: Obj2Dom
+
+### General Dom Elements
+
+```javascript
+let dom = new Obj2Dom({
   type: "div",
-  class: ["element", "parent"],
-  attributes: {
-    id: "parentElement",
-  },
-  events: {
-    click: function () {
-      console.log("clicked on Parent");
-    },
-  },
+  class: "input-wrap",
   nodes: [
     {
-      type: "span",
-      class: "child",
-      innerHTML: "Hello ",
+      type: "div",
+      nodes: [
+        { type: "label", nodes: "Name :" },
+        {
+          type: "input",
+          class: "from-control",
+          attribute: {
+            type: "text",
+          },
+          events: {
+            change: (event) => {
+              console.log("Changed to : " + event.target.value);
+            },
+          },
+        },
+      ],
+    },
+  ], //End Child node of main Div
+});
+
+dom.render(document.getElementById("app"));
+```
+
+### Table
+
+```javascript
+let table = {
+  type: "table",
+  class: ["table", "table-data"],
+  attributes: { border: 1 },
+  nodes: [
+    {
+      type: "thead",
+      nodes: [
+        {
+          type: "tr",
+          nodes: [
+            { type: "th", nodes: "Name" },
+            { type: "th", nodes: "Email" },
+            { type: "th", nodes: "Mobile" },
+          ],
+        },
+      ],
     },
     {
-      type: "span",
-      class: "child",
-      innerHTML: " world ",
+      type: "tbody",
+      nodes: [
+        {
+          type: "tr",
+          nodes: [
+            { type: "td", nodes: "Jhon Doe" },
+            { type: "td", nodes: "jhon@dummy.com" },
+            { type: "td", nodes: "+02126625563" },
+          ],
+        },
+      ],
     },
   ],
-});
-dom.render(document.getElementById("wrap"));
+};
+
+let dom = new Obj2Dom(table);
+dom.render(document.getElementById("app"));
 ```
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-class Dombuilder {
+class Obj2Dom {
   constructor(object) {
+    this.dom = null;
     this.object = object;
     this.init();
   }
@@ -7,42 +8,83 @@ class Dombuilder {
     this.dom = this.buildDom(this.object);
   }
   buildDom(obj) {
-    let elm = document.createElement(obj.type);
-    //Class
-    if (typeof obj.class === "string") {
-      elm.classList.add(obj.class); //Loop For Multiple class
-    } else {
-      obj.class.forEach((v) => {
-        elm.classList.add(v);
-      });
-    }
-    //Attribute
-    if (obj.hasOwnProperty("attributes")) {
-      for (const AttName in obj.attributes) {
-        elm.setAttribute(AttName, obj.attributes[AttName]);
+    if (obj.hasOwnProperty("type")) {
+      let elm = document.createElement(obj.type);
+
+      //Class
+      if (obj.hasOwnProperty("class")) {
+        if (typeof obj.class === "string") {
+          elm.classList.add(obj.class); //Loop For Multiple class
+        } else {
+          obj.class.forEach((v) => {
+            elm.classList.add(v);
+          });
+        }
       }
-    }
-    //Attribute
-    if (obj.hasOwnProperty("events")) {
-      for (const eventName in obj.events) {
-        elm.addEventListener(eventName, obj.events[eventName]);
+      //Attribute
+      if (obj.hasOwnProperty("attributes")) {
+        for (const AttName in obj.attributes) {
+          elm.setAttribute(AttName, obj.attributes[AttName]);
+        }
       }
+      //Events
+      if (obj.hasOwnProperty("events")) {
+        for (const eventName in obj.events) {
+          elm.addEventListener(eventName, obj.events[eventName]);
+        }
+      }
+      //Child Node or Inner Html
+      if (obj.hasOwnProperty("nodes")) {
+        if (typeof obj.nodes === "string") {
+          elm.innerHTML = obj.nodes;
+        } else {
+          obj.nodes.forEach((node) => {
+            elm.appendChild(this.buildDom(node));
+          });
+        }
+      }
+      return elm;
     }
-    //innetHTML
-    if (obj.hasOwnProperty("innerHTML")) {
-      elm.innerHTML = obj.innerHTML;
-    }
-    //Child Node
-    if (obj.hasOwnProperty("nodes")) {
-      obj.nodes.forEach((node) => {
-        elm.appendChild(this.buildDom(node));
-      });
-    }
-    return elm;
+    return null;
   }
   render(domElement) {
     domElement.appendChild(this.dom);
   }
 }
 
-export default Dombuilder;
+class Dombuilder {
+  constructor(tag) {
+    this.element = document.createElement(tag);
+    return this;
+  }
+  attr(name, value) {
+    this.element.setAttribute(name, value);
+    return this;
+  }
+  class(className) {
+    this.element.classList.add(className);
+    return this;
+  }
+  event(eventName, callback) {
+    this.element.addEventListener(eventName, callback);
+    return this;
+  }
+  append(dom) {
+    this.element.appendChild(dom);
+    return this;
+  }
+  renderTo(dom) {
+    dom.appendChild(this.element);
+    return this;
+  }
+  html(string) {
+    this.element.innerHTML = string;
+    return this;
+  }
+  appendHtml(string) {
+    this.element.innerHTML += string;
+    return this;
+  }
+}
+
+export { Dombuilder, Obj2Dom };
